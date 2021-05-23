bfe05f4ba1eaacc9dd17c5e9ec7b03048f93f44b
lipan <panli@yz-intelligence.com>
2021-05-23 17:19:28 +0800

    111

--- src/main/java/com/example/demotke/DemoTkeApplication.java
+++ src/main/java/com/example/demotke/DemoTkeApplication.java
@@ -16,7 +16,7 @@ public class DemoTkeApplication {
 
     @GetMapping("name/{name}")
     public String getName( @PathVariable String name ) {
-        return "hello " +name;
+        return "hello welcom to honghong " +name;
     }
 
 }
