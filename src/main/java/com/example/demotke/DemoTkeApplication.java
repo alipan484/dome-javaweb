369afe74b9f37e3dd9912b54fb8b26fa275e3872
lipan <panli@yz-intelligence.com>
2021-05-23 11:51:49 +0800

    Initial commit

---
+++ src/main/java/com/example/demotke/DemoTkeApplication.java
@@ -0,0 +1,22 @@
+package com.example.demotke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoTkeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTkeApplication.class, args);
+    }
+
+    @GetMapping("name/{name}")
+    public String getName( @PathVariable String name ) {
+        return "hello " +name;
+    }
+
+}
---
+++ src/test/java/com/example/demotke/DemoTkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demotke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTkeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
